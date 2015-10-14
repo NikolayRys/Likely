1aa145edf1510567e9c24d9027060d102700fdb5
volter9 <volter925@gmail.com>
2015-10-14 10:48:23 -0700

    Bower main, and add minified css and js files

--- gulpfile.js
+++ gulpfile.js
@@ -11,31 +11,33 @@ var stylus = require ('gulp-stylus') // https://www.npmjs.com/package/gulp-stylu
 var csso = require ('gulp-csso')
 var zip = require ('gulp-zip')
 
+var release = './release/'
+
 gulp.task ('js', function () {
   return gulp.src ('./source/likely.js')
     .pipe (browserify ())
     .pipe (uglify ())
     .pipe (insert.prepend (require ('fs').readFileSync ('./source/header.js') + '\n'))
-    .pipe (gulp.dest ('../release/'))
+    .pipe (gulp.dest (release))
 })
 
 gulp.task ('css', function () {
   return gulp.src ('./styles/likely.styl')
     .pipe (stylus ())
     .pipe (csso ())
-    .pipe (gulp.dest ('../release/'))
+    .pipe (gulp.dest (release))
 })
 
 gulp.task ('zip', ['js', 'css'], function () {
   var version = require('./package.json').version;
   
   return gulp.src ([
-    '../release/license.txt',
-    '../release/likely.css',
-    '../release/likely.js',
+    release + 'license.txt',
+    release + 'likely.css',
+    release + 'likely.js',
   ])
     .pipe (zip ('ilya-birman-likely-' + version + 'zip'))
-    .pipe (gulp.dest ('../release/'))
+    .pipe (gulp.dest (release))
 })
 
 gulp.task ('build', ['js', 'css'])
@@ -44,5 +46,5 @@ gulp.task ('default', ['js', 'css', 'zip'], function () {
   gulp.watch ('source/*.js', ['zip'])
   gulp.watch ('source/services/*.js', ['zip'])
   gulp.watch ('styles/*.styl', ['zip'])
-  gulp.watch ('../release/license.txt', ['zip'])
+  gulp.watch ('license.txt', ['zip'])
 })
