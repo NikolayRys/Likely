b3b17832bd43a042d28476b7b4514f514f852c76
Laurence Dorman <ljf.dorman@gmail.com>
2016-01-31 23:01:06 +0100

    fix: dynamic protocol in linkedin service

--- source/services/linkedin.js
+++ source/services/linkedin.js
@@ -2,12 +2,14 @@
  * LinkedIn service provider
  */
 
+var config = require('../config');
+
 module.exports = {
-    counterUrl: 'http://www.linkedin.com/countserv/count/share?url={url}&format=jsonp&callback=?',
+    counterUrl: config.protocol + '//www.linkedin.com/countserv/count/share?url={url}&format=jsonp&callback=?',
     convertNumber: function (counter) {
         return counter.count;
     },
-    popupUrl: 'https://www.linkedin.com/shareArticle?url={url}&mini=true&title={title}',
+    popupUrl: config.protocol + '://www.linkedin.com/shareArticle?url={url}&mini=true&title={title}',
     popupWidth: 600,
     popupHeight: 500
 };
