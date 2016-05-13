4889e167a30b6b367b17b4181aefb1435756bb8e
Ivan Akulov <gxoptg@gmail.com>
2016-05-13 20:58:55 +0300

    Update the header used for compiled JS
    * Change the website format
    * Mention Ivan Akulov, Viktor Karpov and other contributors

--- source/header.js
+++ source/header.js
@@ -1,3 +1,4 @@
-//! Likely $version by Ilya Birman, ilyabirman.net
-//! Rewritten sans jQuery by Evgeny Steblinsky, volter9.github.io
-//! Inspired by Social Likes by Artem Sapegin, http://sapegin.me
+//! Likely $version by Ilya Birman (ilyabirman.net)
+//! Rewritten sans jQuery by Evgeny Steblinsky (volter9.github.io)
+//! Supported by Ivan Akulov (iamakulov.com), Viktor Karpov (vitkarpov.com), and contributors
+//! Inspired by Social Likes by Artem Sapegin (sapegin.me)
