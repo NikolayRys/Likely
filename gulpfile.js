1b17940ba7c5fbdef43974f4553d4be88eba3ed1
Ivan Akulov <gxoptg@gmail.com>
2016-05-18 12:05:13 +0300

    Fix the code style issues in Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,56 +3,63 @@
 // "gulp-sass": "^1.1.0",
 // "postcss-assets": "^0.9.0"
 
-var gulp = require ('gulp')
-var browserify = require ('gulp-browserify')
-var uglify = require ('gulp-uglify')
-var insert = require ('gulp-insert')
-var stylus = require ('gulp-stylus')
-var csso = require ('gulp-csso')
-var zip = require ('gulp-zip')
+'use strict';
 
-var release = './release/'
+var gulp = require('gulp');
+var fs = require('fs');
+var browserify = require('gulp-browserify');
+var uglify = require('gulp-uglify');
+var insert = require('gulp-insert');
+var stylus = require('gulp-stylus');
+var csso = require('gulp-csso');
+var zip = require('gulp-zip');
 
-function comment (version) {
-    return require ('fs').readFileSync ('./source/header.js')
-                         .toString ()
-                         .replace (/\$version/g, version)
+var packageJson = require('./package.json');
+
+var release = './release/';
+
+function comment(version) {
+    /* eslint-disable no-sync */
+    return fs.readFileSync('./source/header.js')
+                         .toString()
+                         .replace(/\$version/g, version);
+    /* eslint-enable no-sync */
 }
 
-gulp.task ('js', function () {
-  var version = require ('./package.json').version
-  
-  return gulp.src ('./source/likely.js')
-    .pipe (browserify ())
-    .pipe (uglify ())
-    .pipe (insert.prepend (comment (version)))
-    .pipe (gulp.dest (release))
-})
+gulp.task('js', function () {
+    var version = packageJson.version;
 
-gulp.task ('css', function () {
-  return gulp.src ('./styles/likely.styl')
-    .pipe (stylus ())
-    .pipe (csso ())
-    .pipe (gulp.dest (release))
-})
+    return gulp.src('./source/likely.js')
+    .pipe(browserify())
+    .pipe(uglify())
+    .pipe(insert.prepend(comment(version)))
+    .pipe(gulp.dest(release));
+});
 
-gulp.task ('zip', ['js', 'css'], function () {
-  var version = require ('./package.json').version
-  
-  return gulp.src ([
-    release + 'license.txt',
-    release + 'likely.css',
-    release + 'likely.js',
-  ])
-    .pipe (zip ('ilya-birman-likely-' + version + '.zip'))
-    .pipe (gulp.dest (release))
-})
+gulp.task('css', function () {
+    return gulp.src('./styles/likely.styl')
+    .pipe(stylus())
+    .pipe(csso())
+    .pipe(gulp.dest(release));
+});
 
-gulp.task ('build', ['js', 'css'])
+gulp.task('zip', ['js', 'css'], function () {
+    var version = packageJson.version;
 
-gulp.task ('default', ['js', 'css', 'zip'], function () {
-  gulp.watch ('source/*.js', ['zip'])
-  gulp.watch ('source/services/*.js', ['zip'])
-  gulp.watch ('styles/*.styl', ['zip'])
-  gulp.watch ('license.txt', ['zip'])
-})
+    return gulp.src([
+        release + 'license.txt',
+        release + 'likely.css',
+        release + 'likely.js',
+    ])
+    .pipe(zip('ilya-birman-likely-' + version + '.zip'))
+    .pipe(gulp.dest(release));
+});
+
+gulp.task('build', ['js', 'css']);
+
+gulp.task('default', ['js', 'css', 'zip'], function () {
+    gulp.watch('source/*.js', ['zip']);
+    gulp.watch('source/services/*.js', ['zip']);
+    gulp.watch('styles/*.styl', ['zip']);
+    gulp.watch('license.txt', ['zip']);
+});
