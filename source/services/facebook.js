a897767ba27bc9b9fa8f2caca800a0d53a4c3070
Ivan Akulov <iamakulov@users.noreply.github.com>
2016-08-23 12:38:47 +0300

    Update the Facebook counter URL (#87)
    FQL was disabled in August 2016.
    
    Closes #86.

--- source/services/facebook.js
+++ source/services/facebook.js
@@ -5,9 +5,9 @@
  */
 
 module.exports = {
-    counterUrl: 'https://graph.facebook.com/fql?q=SELECT+total_count+FROM+link_stat+WHERE+url%3D%22{url}%22&callback=?',
-    convertNumber: function (counter) {
-        return counter.data[0].total_count;
+    counterUrl: 'https://graph.facebook.com/?id={url}&callback=?',
+    convertNumber: function (data) {
+        return data.share.share_count;
     },
     popupUrl: 'https://www.facebook.com/sharer/sharer.php?u={url}',
     popupWidth: 600,
