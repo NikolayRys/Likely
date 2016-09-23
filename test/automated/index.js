41e5df66dc6a2019e8ea920075c1a3ffbdcb9876
Ivan Akulov <gxoptg@gmail.com>
2016-09-24 02:19:05 +0300

    Rewrite expectClickToOpen()
    * Remove the unnecessary Promise nesting
    * Close the dialog and switch back to the main window before comparing the URL
    * Make the method work with Firefox

--- test/automated/index.js
+++ test/automated/index.js
@@ -205,25 +205,45 @@ function expectToContainText(driver, selector, value) {
 
 function expectClickToOpen(driver, clickTargetSelector, windowUrlRegex) {
     let originalWindowHandle;
+    let openedUrl;
     return driver.findElement({css: clickTargetSelector}).click()
         .then(function () {
             return Promise.all([
                 driver.getWindowHandle(),
                 driver.getAllWindowHandles()
-            ]).then(function ([currentHandle, handles]) {
-                originalWindowHandle = currentHandle;
+            ]);
+        })
+        .then(function ([currentHandle, handles]) {
+            originalWindowHandle = currentHandle;
 
-                const newWindowHandle = handles.filter(h => h !== currentHandle)[0];
-                return driver.switchTo().window(newWindowHandle);
-            });
+            const newWindowHandle = handles.find(handle => handle !== currentHandle);
+            return driver.switchTo().window(newWindowHandle);
         })
+        // `driver.wait()` is used because Firefox opens a new window with `about:blank' initially
         .then(function () {
-            return expect(driver.getCurrentUrl()).to.eventually.match(windowUrlRegex);
+            // This time should be enough for Firefox to load something and replace `about:blank` with the target URL
+            const urlChangeTimeout = 3000;
+            return driver.wait(until.urlMatches(windowUrlRegex), urlChangeTimeout);
         })
-        .then(function () {
+        // `driver.wait()` triggers an exception if the url doesn’t match the regex,
+        // but we actually ignore this exception and do the proper URL comparison later
+        .then(
+            () => driver.getCurrentUrl(),
+            () => driver.getCurrentUrl()
+        )
+        .then(function (url) {
+            openedUrl = url;
+
             return driver.close();
         })
         .then(function () {
             return driver.switchTo().window(originalWindowHandle);
+        })
+        // We compare the URLs only after closing the dialog and switching back to the main window.
+        // If we do it before and the comparison fails, all the `.then()` branches won’t execute,
+        // and Selenium will continue working in the opened dialog.
+        // This will make all the following tests fail
+        .then(function () {
+            expect(openedUrl).to.match(windowUrlRegex);
         });
 }
