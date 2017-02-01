29edc2c4e56d9c96fcbad1e775c1956fb8e3feb7
Ivan Akulov <gxoptg@gmail.com>
2017-02-01 22:02:48 +0300

    Inline the SVG icons into the service descriptors

--- source/services/facebook.js
+++ source/services/facebook.js
@@ -8,4 +8,5 @@ export default {
     popupUrl: 'https://www.facebook.com/sharer/sharer.php?u={url}',
     popupWidth: 600,
     popupHeight: 500,
+    svgi: '13 0H3C1 0 0 1 0 3v10c0 2 1 3 3 3h5V9H6V7h2V5c0-2 2-2 2-2h3v2h-3v2h3l-.5 2H10v7h3c2 0 3-1 3-3V3c0-2-1-3-3-3',
 };
