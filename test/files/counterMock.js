923085c8b6653a1a3758ba590491fc69451e3a00
nikolay <nikolay@rys.me>
2020-05-20 06:11:46 +0200

    Simplify counterMock

--- test/files/counterMock.js
+++ test/files/counterMock.js
@@ -1,9 +1,5 @@
 'use strict'; // eslint-disable-line strict
 
-window.__likelyCounterMock = function (url, callback) {
-    // The callback (which is actually a factory from `factory.js`)
-    // doesn’t save the passed argument if it’s passed synchronously.
-    setTimeout(function () {
-        callback(10);
-    }, 0);
+window.__likelyFetchMock = function (updateBroadcaster) {
+    updateBroadcaster.trigger(10);
 };
