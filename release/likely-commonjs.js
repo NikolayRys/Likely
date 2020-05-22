afa783c10a62c5e271feb16b9113bdf9ec26ff4c
nikolay <nikolay@rys.me>
2020-05-22 21:01:25 +0200

    Update build

--- release/likely-commonjs.js
+++ release/likely-commonjs.js
@@ -80,7 +80,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 21);
+/******/ 	return __webpack_require__(__webpack_require__.s = 23);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -217,7 +217,7 @@ var bools = function bools(node) {
         if (Object.prototype.hasOwnProperty.call(data, key)) {
             var value = data[key];
 
-            result[key] = bool[value] || value;
+            result[key] = value in bool ? bool[value] : value;
         }
     }
 
@@ -320,7 +320,7 @@ var getDefaultUrl = function getDefaultUrl() {
  * Is code run in browser or on server.
  */
 var isBrowserEnv = typeof window !== 'undefined' && typeof document !== 'undefined' && document.createElement;
-/* WEBPACK VAR INJECTION */}.call(__webpack_exports__, __webpack_require__(19)))
+/* WEBPACK VAR INJECTION */}.call(__webpack_exports__, __webpack_require__(21)))
 
 /***/ }),
 /* 1 */
@@ -400,7 +400,7 @@ var loadScript = function loadScript(url) {
 var getJSON = function getJSON(url, callback) {
     var name = encodeURIComponent('random_fun_' + ++gid);
 
-    var concreteUrl = url.replace(/callback=(\?)/, 'callback=__likelyCallbacks.' + name);
+    var concreteUrl = url.replace(/(callback|jsonp)=(\?)/, '$1=__likelyCallbacks.' + name);
 
     global.__likelyCallbacks[name] = callback;
 
@@ -503,10 +503,12 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__linkedin__ = __webpack_require__(10);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__odnoklassniki__ = __webpack_require__(11);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__pinterest__ = __webpack_require__(12);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__telegram__ = __webpack_require__(13);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__twitter__ = __webpack_require__(14);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__vkontakte__ = __webpack_require__(15);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__whatsapp__ = __webpack_require__(16);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__telegram__ = __webpack_require__(14);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__twitter__ = __webpack_require__(15);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__vkontakte__ = __webpack_require__(17);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__whatsapp__ = __webpack_require__(18);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_10__viber__ = __webpack_require__(16);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_11__reddit__ = __webpack_require__(13);
 /**
  * Social network services
  */
@@ -514,6 +516,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 /* eslint-disable sort-imports */
+
+
 
 
 
@@ -532,7 +536,9 @@ var services = {
     telegram: __WEBPACK_IMPORTED_MODULE_6__telegram__["a" /* default */],
     twitter: __WEBPACK_IMPORTED_MODULE_7__twitter__["a" /* default */],
     vkontakte: __WEBPACK_IMPORTED_MODULE_8__vkontakte__["a" /* default */],
-    whatsapp: __WEBPACK_IMPORTED_MODULE_9__whatsapp__["a" /* default */]
+    whatsapp: __WEBPACK_IMPORTED_MODULE_9__whatsapp__["a" /* default */],
+    viber: __WEBPACK_IMPORTED_MODULE_10__viber__["a" /* default */],
+    reddit: __WEBPACK_IMPORTED_MODULE_11__reddit__["a" /* default */]
 };
 
 __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__utils__["each"])(services, function (service, key) {
@@ -559,14 +565,14 @@ var _require = __webpack_require__(0),
     getDefaultUrl = _require.getDefaultUrl,
     merge = _require.merge;
 
-var Likely = __webpack_require__(17).default;
+var Likely = __webpack_require__(19).default;
 var config = __webpack_require__(2).default;
 
 var _require2 = __webpack_require__(1),
     findAll = _require2.findAll;
 
 var history = __webpack_require__(7).default;
-__webpack_require__(18);
+__webpack_require__(20);
 
 /**
  * @param {Node} node
@@ -811,7 +817,7 @@ var LikelyButton = function () {
         key: 'registerAsCounted',
         value: function registerAsCounted() {
             var options = this.options;
-            if (options.counterUrl) {
+            if (options.counters && options.counterUrl) {
                 __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_3__connectButtonToService__["a" /* default */])(this.serviceName, this.setDisplayedCounter.bind(this), options);
             }
         }
@@ -873,7 +879,8 @@ var LikelyButton = function () {
             if (options.click.call(this)) {
                 var url = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__utils__["interpolateUrl"])(options.popupUrl, {
                     url: options.url,
-                    title: options.title
+                    title: options.title,
+                    content: options.content
                 });
 
                 if (options.openPopup === false) {
@@ -1146,6 +1153,32 @@ __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__utils__["registerGlobalCallba
 
 "use strict";
 /**
+ * Reddit service provider
+ *
+ * Notes:
+ * 1) How are shares counted
+ * The number of upvotes minus the number of down votes for a specific URL submitted on Reddit
+ * `children[0].data.score` - contain pre calculated value
+ */
+/* harmony default export */ __webpack_exports__["a"] = ({
+    counterUrl: 'https://www.reddit.com/api/info.json?url={url}&limit=1&jsonp=?',
+    convertNumber: function convertNumber(response) {
+        var children = response.data.children[0] || {};
+
+        return children.data.score ? children.data.score : undefined;
+    },
+    popupUrl: 'https://reddit.com/submit?url={url}&title={title}',
+    popupWidth: 600,
+    popupHeight: 500,
+    svgIconPath: '16 8 C 16 12.418 12.418 16 8 16 C 3.582 16 0 12.418 0 8 C 0 3.582 3.582 0 8 0 C 12.418 0 16 3.582 16 8 Z  M 9.836 9.665 C 9.357 9.665 8.965 9.241 9.001 8.764 C 9.034 8.336 9.406 7.991 9.836 7.991 C 10.314 7.991 10.707 8.415 10.67 8.892 C 10.637 9.32 10.265 9.665 9.836 9.665 L 9.836 9.665 Z  M 9.98 11.033 C 9.413 11.6 8.324 11.645 8 11.645 C 7.676 11.645 6.587 11.6 6.02 11.033 C 5.939 10.952 5.939 10.808 6.02 10.727 C 6.101 10.646 6.245 10.646 6.326 10.727 C 6.686 11.087 7.451 11.213 8 11.213 C 8.549 11.213 9.314 11.087 9.674 10.727 C 9.755 10.646 9.899 10.646 9.98 10.727 C 10.061 10.808 10.061 10.952 9.98 11.033 Z  M 5.336 8.828 C 5.336 8.35 5.76 7.957 6.237 7.993 C 6.666 8.026 7.01 8.398 7.01 8.828 C 7.01 9.306 6.586 9.699 6.109 9.662 C 5.681 9.629 5.336 9.257 5.336 8.828 L 5.336 8.828 Z  M 13.336 7.991 C 13.336 7.343 12.814 6.821 12.166 6.821 C 11.852 6.821 11.564 6.947 11.357 7.145 C 10.556 6.569 9.458 6.2 8.234 6.155 L 8.765 3.654 L 10.502 4.022 C 10.52 4.463 10.889 4.814 11.33 4.814 C 11.789 4.814 12.166 4.445 12.166 3.978 C 12.166 3.519 11.798 3.141 11.33 3.141 C 11.006 3.141 10.718 3.33 10.583 3.609 L 8.648 3.195 C 8.537 3.176 8.424 3.241 8.405 3.357 L 8.405 3.357 L 7.811 6.146 C 6.569 6.182 5.453 6.551 4.643 7.136 C 4.436 6.938 4.148 6.812 3.834 6.812 C 3.186 6.812 2.664 7.334 2.664 7.982 C 2.664 8.459 2.943 8.864 3.357 9.044 C 3.339 9.161 3.33 9.278 3.33 9.395 C 3.33 11.186 5.417 12.643 8 12.643 C 10.574 12.643 12.67 11.186 12.67 9.395 C 12.67 9.278 12.661 9.161 12.643 9.044 C 13.048 8.864 13.336 8.45 13.336 7.982 L 13.336 7.991'
+});
+
+/***/ }),
+/* 14 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/**
  * Telegram service provider
  */
 
@@ -1157,7 +1190,7 @@ __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__utils__["registerGlobalCallba
 });
 
 /***/ }),
-/* 14 */
+/* 15 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -1182,7 +1215,32 @@ __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__utils__["registerGlobalCallba
 });
 
 /***/ }),
-/* 15 */
+/* 16 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/**
+ * Viber service provider
+ */
+
+/* harmony default export */ __webpack_exports__["a"] = ({
+    popupUrl: 'viber://forward?text={content}',
+    click: function click() {
+        if (Object.prototype.hasOwnProperty.call(this.widget.dataset, 'comment')) {
+            this.options.content = this.options.url + '\n' + this.widget.dataset.comment;
+        } else {
+            this.options.content = this.options.url;
+        }
+
+        return true;
+    },
+
+    openPopup: false,
+    svgIconPath: '8.52,6.76 C 8.52,6.81 8.66,6.91 8.85,6.97 9.02,7.03 9.10,7.01 9.04,6.89 8.91,6.68 8.52,6.60 8.52,6.76 Z M 5.88,5.16 C 5.03,5.53 4.63,6.14 4.47,7.39 4.28,8.79 4.63,10.01 5.40,10.57 5.92,10.94 6.03,11.16 6.03,11.83 6.03,11.83 6.05,12.66 6.05,12.66 6.05,12.66 6.71,11.89 6.71,11.89 7.29,11.24 7.50,11.14 8.31,11.12 10.60,11.10 11.51,10.27 11.51,8.16 11.53,6.34 11.20,5.59 10.16,5.18 9.14,4.78 6.79,4.78 5.88,5.16 Z M 9.56,6.22 C 10.22,6.81 10.68,8.30 10.20,8.30 10.08,8.30 9.97,8.06 9.95,7.74 9.95,7.74 9.93,7.19 9.93,7.19 9.93,7.19 9.77,7.70 9.77,7.70 9.77,7.70 9.62,8.20 9.62,8.20 9.62,8.20 9.39,7.70 9.39,7.70 9.21,7.29 9.16,7.27 9.16,7.57 9.14,7.94 9.10,7.92 8.66,7.47 8.39,7.21 8.23,6.91 8.27,6.81 8.33,6.72 8.23,6.56 8.08,6.46 7.88,6.34 7.94,6.28 8.29,6.28 8.58,6.28 9.04,6.48 9.33,6.72 9.33,6.72 9.87,7.19 9.87,7.19 9.87,7.19 9.37,6.62 9.37,6.62 9.08,6.32 8.64,6.08 8.39,6.08 8.12,6.08 7.85,5.97 7.79,5.87 7.50,5.41 8.96,5.69 9.56,6.22 Z M 7.00,6.40 C 7.25,6.78 7.29,7.01 7.13,7.27 6.63,8.02 7.92,9.36 8.71,8.87 9.02,8.69 9.21,8.71 9.64,8.97 10.29,9.40 10.33,9.74 9.79,10.21 9.41,10.53 9.35,10.53 8.37,10.07 6.44,9.09 5.05,6.91 5.86,6.12 6.25,5.73 6.61,5.83 7.00,6.40 Z M 6.59,0.61 C 4.09,1.03 1.52,3.10 0.62,5.41 -0.02,7.05 -0.02,9.38 0.62,11.04 1.29,12.74 3.35,14.74 5.09,15.39 6.82,16.02 9.18,16.02 10.91,15.39 12.65,14.74 14.71,12.74 15.38,11.04 16.02,9.36 16.02,7.05 15.38,5.37 14.73,3.71 12.63,1.66 11.01,1.07 9.60,0.57 7.92,0.38 6.59,0.61 Z M 10.81,4.58 C 11.97,5.23 12.34,6.10 12.32,8.02 12.32,10.71 11.41,11.71 8.77,11.89 7.65,11.97 7.27,12.09 6.82,12.52 6.13,13.12 5.61,13.00 5.61,12.21 5.61,11.81 5.44,11.54 5.01,11.30 4.03,10.77 3.66,9.86 3.66,8.00 3.66,4.94 4.80,3.99 8.33,4.11 9.64,4.15 10.29,4.27 10.81,4.58'
+});
+
+/***/ }),
+/* 17 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -1219,7 +1277,7 @@ __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__utils__["registerGlobalCallba
 /* harmony default export */ __webpack_exports__["a"] = (vkontakte);
 
 /***/ }),
-/* 16 */
+/* 18 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -1238,7 +1296,7 @@ __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__utils__["registerGlobalCallba
 });
 
 /***/ }),
-/* 17 */
+/* 19 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -1279,6 +1337,7 @@ var Likely = function () {
             this.timeout = setTimeout(this.ready.bind(this), this.options.timeout);
         } else {
             this.appear();
+            this.ready();
         }
         this.materializeButtons();
     }
@@ -1359,11 +1418,8 @@ var Likely = function () {
     }, {
         key: 'ready',
         value: function ready() {
-            if (this.timeout) {
-                clearTimeout(this.timeout);
-
-                this.container.classList.add(__WEBPACK_IMPORTED_MODULE_1__config__["default"].name + '_ready');
-            }
+            clearTimeout(this.timeout);
+            this.container.classList.add(__WEBPACK_IMPORTED_MODULE_1__config__["default"].name + '_ready');
         }
     }]);
 
@@ -1373,13 +1429,13 @@ var Likely = function () {
 /* harmony default export */ __webpack_exports__["default"] = (Likely);
 
 /***/ }),
-/* 18 */
+/* 20 */
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
 
 /***/ }),
-/* 19 */
+/* 21 */
 /***/ (function(module, exports) {
 
 var g;
@@ -1406,8 +1462,8 @@ module.exports = g;
 
 
 /***/ }),
-/* 20 */,
-/* 21 */
+/* 22 */,
+/* 23 */
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = __webpack_require__(4);
