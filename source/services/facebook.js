14db0900cd3a7a4a17eb05fa3463acefea381d1f
nikolay <nikolay@rys.me>
2020-09-28 06:42:59 +0200

    Rework facebook
    JSONP is removed, documentation is added, API token for Likely is obtained, window size is adjusted

--- source/services/facebook.js
+++ source/services/facebook.js
@@ -1,13 +1,17 @@
 /**
  * Facebook service provider
+ * Share doc: https://developers.facebook.com/docs/workplace/sharing/share-dialog/
+ * Counter doc: https://developers.facebook.com/docs/graph-api/reference/v8.0/engagement
+ *
+ * For hackers: the access token below is public, heavily restricted and doesn't allow to access anything of value
  */
 
 export default {
-    counterUrl: 'https://graph.facebook.com/?id={url}&fields=og_object%7Bengagement%7Bcount%7D%7D&callback=?',
-    convertNumber: (data) => data.og_object.engagement.count,
-    popupUrl: 'https://www.facebook.com/sharer/sharer.php?u={url}',
-    popupWidth: 600,
-    popupHeight: 500,
-    knownParams: ['url'],
+    counterUrl: 'https://graph.facebook.com/?id={url}&access_token=1729830587180291|102e6d79cda2fa63b65c99c039eed12a&fields=og_object%7Bengagement%7Bcount%7D%7D',
+    convertNumber: (data) => JSON.parse(data).og_object.engagement.count,
+    popupWidth: 555,
+    popupHeight: 555,
+    popupUrl: 'https://www.facebook.com/sharer.php?u={url}',
+    knownParams: ['url', 'quote'],
     svgIconPath: '16.000,8.049 C16.000,3.629 12.418,0.047 8.000,0.047 C3.582,0.047 -0.000,3.629 -0.000,8.049 C-0.000,12.043 2.925,15.353 6.750,15.953 L6.750,10.362 L4.719,10.362 L4.719,8.049 L6.750,8.049 L6.750,6.286 C6.750,4.280 7.944,3.173 9.772,3.173 C10.647,3.173 11.563,3.329 11.563,3.329 L11.563,5.298 L10.554,5.298 C9.560,5.298 9.250,5.915 9.250,6.548 L9.250,8.049 L11.469,8.049 L11.114,10.362 L9.250,10.362 L9.250,15.953 C13.075,15.353 16.000,12.043 16.000,8.049',
 };
