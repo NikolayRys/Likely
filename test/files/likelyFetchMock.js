da684f56c38e9b904d0d16904aa3660dfbb2681a
Nikolay Rys <nikolay@rys.me>
2022-07-31 17:54:59 +0300

    Give name to magic value

--- test/files/likelyFetchMock.js
+++ test/files/likelyFetchMock.js
@@ -2,6 +2,8 @@
 
 window.__likelyFetchMock = function (broadcaster) {
     window.__likelyFetchMock.calls++;
-    broadcaster.trigger(10);
+    broadcaster.trigger(window.__likelyFetchMock.stubbedValue);
 };
+
+window.__likelyFetchMock.stubbedValue = 10;
 window.__likelyFetchMock.calls = 0;
