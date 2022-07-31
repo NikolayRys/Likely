f036530821527a1f38991ffe5e0ebf0eb5ee184b
Nikolay Rys <nikolay@rys.me>
2022-07-31 17:29:55 +0300

    Make more robust likely test initialization

--- test/utils/waitUntilLikelyInitialized.js
+++ test/utils/waitUntilLikelyInitialized.js
@@ -3,7 +3,9 @@
 const until = require('selenium-webdriver/lib/until');
 
 function waitUntilLikelyInitialized(driver) {
-    return driver.wait(until.elementLocated({ css: '.likely_ready' }), 1500);
+    return driver.wait(until.elementLocated({ css: '.likely_ready' }), 500)
+        .then(() => driver.findElement({ css: '.likely_ready' }))
+        .then((el) => driver.wait(until.elementIsVisible(el), 500));
 }
 
 module.exports = waitUntilLikelyInitialized;
