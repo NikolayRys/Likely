d5093e25ba53e7719e57770f06a4698013ecb945
Nikolay Rys <nikolay@rys.me>
2022-07-31 23:24:21 +0300

    Do not load styles into commonjs build

--- source/likely.js
+++ source/likely.js
@@ -2,6 +2,7 @@
 // It’s written with CommonJS imports and exports to make possible doing `module.exports = likely`.
 // This is required so that users work with `window.likely`, not `window.likely.default`
 
+require('./index.styl');
 const likely = require('./likely-commonjs.js');
 
 window.addEventListener('load', () => {
