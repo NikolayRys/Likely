d6159cd4123cbd91ea7f09e678d2feb764537093
Nikolay Rys <nikolay@rys.me>
2024-05-07 04:51:13 +0300

    Fix doc param style

--- source/dom.js
+++ source/dom.js
@@ -8,8 +8,8 @@ const div = isBrowserEnv ? document.createElement('div') : {};
 
 /**
  * Wrap SVG coords from data object into SVG tag
- * @param {String} coords
- * @returns {String}
+ * @param {string} coords
+ * @returns {string}
  */
 export const wrapSVG = (coords) =>
     '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" ' +
@@ -17,7 +17,7 @@ export const wrapSVG = (coords) =>
 
 /**
  * Create node from HTML
- * @param {String} html
+ * @param {string} html
  * @returns {Node}
  */
 export const createNode = (html) => {
@@ -28,7 +28,7 @@ export const createNode = (html) => {
 
 /**
  * Load JSONP script. It gets executed after the main one is finished.
- * @param {String} url
+ * @param {string} url
  */
 export const loadJSONP = (url) => {
     const script = document.createElement('script');
@@ -43,7 +43,7 @@ export const loadJSONP = (url) => {
 
 /**
  * Find first node by selector
- * @param {String} selector
+ * @param {string} selector
  * @param {Node} [node]
  * @returns {Node}
  */
@@ -51,18 +51,18 @@ export const find = (selector, node) => (node || document).querySelector(selecto
 
 /**
  * Find all nodes by selector
- * @param {String} selector
+ * @param {string} selector
  * @param {Node} [node]
  * @returns {Node[]}
  */
 export const findAll = (selector, node) => Array.prototype.slice.call((node || document).querySelectorAll(selector));
 /**
  * Open the popup
- * @param {String} url
- * @param {String} winId
- * @param {Number} width,
- * @param {Number} height
- * @returns {Object|null}
+ * @param {string} url
+ * @param {string} winId
+ * @param {number} width
+ * @param {number} height
+ * @returns {object | null}
  */
 export const openPopup = (url, winId, width, height) => {
     const left = Math.round(screen.width / 2 - width / 2);
